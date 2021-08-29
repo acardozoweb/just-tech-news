07df2fd374d8f701836eb07cbb588c45ebf5051c
anthonycc <anthonyccardozo@gmail.com>
2021-08-28 20:56:54 -0400

    fixed comment routes

--- server.js
+++ server.js
@@ -12,7 +12,8 @@ app.use(express.urlencoded({ extended: true }));
 app.use(routes);
 
 // turn on connection to db and server
-sequelize.sync({ force: true }).then(() => {
+// force:true so the tables drop and recreate upon server start
+sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log('Now listening'));
 });
 
